a18a3c196d544bbef7ddd4e42b8fbc1f91d5449b
Yana Soboleva <luxuriouslystellarsky@gmail.com>
2026-04-19 00:49:16 +0300

    Update api1

--- app/(private routes)/notes/filter/@sidebar/default.tsx
+++ app/(private routes)/notes/filter/@sidebar/default.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import { NoteTag } from '@/lib/api';
+import { NoteTag } from '@/lib/api/api';
 import css from './SidebarNotes.module.css';
 
 const TAGS: NoteTag[] = ['Work', 'Personal', 'Meeting', 'Shopping', 'Todo'];
